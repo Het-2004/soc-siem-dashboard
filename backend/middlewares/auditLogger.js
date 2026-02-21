cfbdc1f8eeac4d0aab1a0c8190624150a390581c
HET SOLANKI <solankihetpravinbhai2122004@gmail.com>
2026-02-21 07:39:18 +0530

    Add Environment to easy starting application

--- backend/middlewares/auditLogger.js
+++ backend/middlewares/auditLogger.js
@@ -2,12 +2,16 @@ const AuditLog = require("../models/AuditLog");
 
 module.exports = async function auditLogger(req, res, next) {
   if (req.user) {
-    await AuditLog.create({
-      action: `${req.method} ${req.originalUrl}`,
-      performedBy: req.user.id,
-      role: req.user.role,
-      ipAddress: req.ip
-    });
+    try {
+      await AuditLog.create({
+        action: `${req.method} ${req.originalUrl}`,
+        performedBy: req.user.id,
+        role: req.user.role,
+        ipAddress: req.ip
+      });
+    } catch (err) {
+      console.error("Audit log error:", err);
+    }
   }
   next();
 };

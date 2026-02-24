c32bd49e8eff9527dc52fb8f6596f72d07cb5939
HET SOLANKI <solankihetpravinbhai2122004@gmail.com>
2026-02-24 22:03:22 +0530

    Harden IP blocker error handling

--- backend/middlewares/ipBlocker.js
+++ backend/middlewares/ipBlocker.js
@@ -1,13 +1,19 @@
 const BlockedIP = require("../models/BlockedIP");
+const logger = require("../utils/logger");
 
 module.exports = async function ipBlocker(req, res, next) {
-  const blocked = await BlockedIP.findOne({ ipAddress: req.ip });
+  try {
+    const blocked = await BlockedIP.findOne({ ipAddress: req.ip });
 
-  if (blocked) {
-    return res.status(403).json({
-      message: "Access denied. Your IP is blocked."
-    });
+    if (blocked) {
+      return res.status(403).json({
+        message: "Access denied. Your IP is blocked."
+      });
+    }
+
+    next();
+  } catch (error) {
+    logger.error("IP block check failed", error);
+    next();
   }
-
-  next();
 };
